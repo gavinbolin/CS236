ba82c72961f759343a08d0bdac1dd61402829987
gavinbolin <gavinbolin3411@gmail.com>
2025-05-05 18:43:17 -0600

    UPLOAD

---
+++ project_0/cmake-build-debug-schizo/CMakeFiles/project_0.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/users/guest/g/gbolin/cs236/Deploy/Book.cpp" "/users/guest/g/gbolin/cs236/Deploy/cmake-build-debug-schizo/CMakeFiles/project_0.dir/Book.cpp.o"
+  "/users/guest/g/gbolin/cs236/Deploy/BookCreator.cpp" "/users/guest/g/gbolin/cs236/Deploy/cmake-build-debug-schizo/CMakeFiles/project_0.dir/BookCreator.cpp.o"
+  "/users/guest/g/gbolin/cs236/Deploy/BookListCreator.cpp" "/users/guest/g/gbolin/cs236/Deploy/cmake-build-debug-schizo/CMakeFiles/project_0.dir/BookListCreator.cpp.o"
+  "/users/guest/g/gbolin/cs236/Deploy/Info.cpp" "/users/guest/g/gbolin/cs236/Deploy/cmake-build-debug-schizo/CMakeFiles/project_0.dir/Info.cpp.o"
+  "/users/guest/g/gbolin/cs236/Deploy/Library.cpp" "/users/guest/g/gbolin/cs236/Deploy/cmake-build-debug-schizo/CMakeFiles/project_0.dir/Library.cpp.o"
+  "/users/guest/g/gbolin/cs236/Deploy/main.cpp" "/users/guest/g/gbolin/cs236/Deploy/cmake-build-debug-schizo/CMakeFiles/project_0.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ project_0/cmake-build-debug-schizo/CMakeFiles/project_0.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_0.dir/Book.cpp.o"
+  "CMakeFiles/project_0.dir/BookCreator.cpp.o"
+  "CMakeFiles/project_0.dir/BookListCreator.cpp.o"
+  "CMakeFiles/project_0.dir/Info.cpp.o"
+  "CMakeFiles/project_0.dir/Library.cpp.o"
+  "CMakeFiles/project_0.dir/main.cpp.o"
+  "project_0"
+  "project_0.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project_0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
